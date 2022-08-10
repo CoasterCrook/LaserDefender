904dade21e56dce6ff370cab1189373640dfe13c
Garrett Crook <GRCrook03@icloud.com>
2022-08-10 13:48:51 -0400

    Implemented shooting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,11 +10,17 @@ public class Player : MonoBehaviour
     [SerializeField] float paddingRight;
     [SerializeField] float paddingTop;
     [SerializeField] float paddingBottom;
+    Shooter shooter;
     
     Vector2 rawInput;
     Vector2 minBounds;
     Vector2 maxBounds;
 
+    void Awake() 
+    {
+        shooter = GetComponent<Shooter>();
+    }
+    
     void Start() 
     {
         InitBounds();
@@ -44,5 +50,13 @@ public class Player : MonoBehaviour
     void OnMove(InputValue value)
     {
         rawInput = value.Get<Vector2>();
+    }
+
+    void OnFire(InputValue value)
+    { 
+        if(shooter != null)
+        {
+            shooter.isFiring = value.isPressed;
+        }
     }
 }
